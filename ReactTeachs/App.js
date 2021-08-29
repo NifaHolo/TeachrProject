73f612097054b899e859f67453b4fff5f3d52a7e
NifaHolo <julien.leconte2002@gmail.com>
2021-08-29 11:49:17 +0200

    Mise en page

--- ReactTeachs/App.js
+++ ReactTeachs/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.containerStyling}>
+        <View style={styles.ArrowContainer}>
+          <View style={{width: 500, backgroundColor: 'rgb(0, 113, 205)'}}>
+            <Text style={styles.Arrow}>&#60;</Text>
+          </View>
+        </View>
 
-        <Text>Teach'rs favoris</Text>
 
+        <View style={styles.textcontainer}>
+          <View
+            style={{
+              width: 500,
+              height: 100,
+              backgroundColor: 'rgb(0, 113, 205)',
+            }}>
+            <Text style={styles.text}>Teach'rs favoris</Text>
+          </View>
+        </View>
       </View>
-    
     );
   }
 }
+const styles = StyleSheet.create({
+  containerStyling: {
+    backgroundColor: 'rgb(0, 113, 205)',
+    height: 200,
+  },
+  textcontainer: {
+    paddingTop: 25,
+  },
+  text: {
+    paddingLeft: 30,
+    fontSize: 30,
+    color: 'white',
+  },
+  ArrowContainer: {
+    paddingTop: 25,
+  },
+  Arrow: {
+    paddingLeft: 30,
+    fontSize: 50,
+    color: 'white',
+  },
+});
